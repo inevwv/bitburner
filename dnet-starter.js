f75f21f8a323c308f0aa8598d2e3286fe6d1a59e
inevwv <109582694+inevwv@users.noreply.github.com>
2026-05-17 19:04:15 -0500

    Update dnet-starter.js

--- dnet-starter.js
+++ dnet-starter.js
@@ -1,7 +1,23 @@
+// dnet-starter.js
+const SCRIPTS = [
+  "dnet-probe.js",
+  "dnet-rider.js",
+  "dnet-interactive-solver.js",
+  "dnet-stasis.js",
+  "dnet-cache-opener.js",
+  "dnet-deploy-rider.js",
+  "dnet-storm.js",
+  "dnet-mapper.js",
+];
+
 export async function main(ns) {
   ns.clearPort(21); // clear rider port so probe can redeploy
-  await ns.scp("dnet-probe.js", "darkweb");
-  await ns.scp("dnet-rider.js", "darkweb");
+
+  // scp all scripts to darkweb
+  for (const script of SCRIPTS) {
+    await ns.scp(script, "darkweb");
+  }
+
   ns.exec("dnet-probe.js", "darkweb");
   ns.exec("dnet-mapper.js", "home");
   ns.tprint("Probe and mapper launched");
