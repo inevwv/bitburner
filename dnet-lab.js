627cba71a3e369891a77cf73ffe1a6d90dd93ff2
inevwv <109582694+inevwv@users.noreply.github.com>
2026-05-17 19:16:19 -0500

    Delete dnet-lab.js

--- dnet-lab.js
+++
@@ -1,7 +0,0 @@
-// dnet-lab.js
-export async function main(ns) {
-  const radar = await ns.dnet.labradar();
-  const report = await ns.dnet.labreport();
-  ns.tprint("RADAR: " + JSON.stringify(radar));
-  ns.tprint("REPORT: " + JSON.stringify(report));
-}
