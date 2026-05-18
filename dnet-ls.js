bb4dc507970b380a709f60182b0fdf9bc2fefe60
inevwv <109582694+inevwv@users.noreply.github.com>
2026-05-17 19:17:52 -0500

    Delete dnet-ls.js

--- dnet-ls.js
+++
@@ -1,6 +0,0 @@
-// dnet-ls.js
-export async function main(ns) {
-  const files = ns.ls(ns.getHostname());
-  ns.tprint(`Files on ${ns.getHostname()}:`);
-  for (const f of files) ns.tprint(`  ${f}`);
-}
