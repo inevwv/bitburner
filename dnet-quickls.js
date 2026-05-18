f9b95dac5aadea0921f8be7a314fbe7433ee9097
inevwv <109582694+inevwv@users.noreply.github.com>
2026-05-17 19:18:15 -0500

    Delete dnet-quickls.js

--- dnet-quickls.js
+++
@@ -1,7 +0,0 @@
-// dnet-quickls.js
-export async function main(ns) {
-  const target = ns.args[0];
-  await ns.scp("dnet-ls.js", target);
-  const pid = ns.exec("dnet-ls.js", target);
-  ns.tail(pid);
-}
